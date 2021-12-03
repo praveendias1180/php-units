33394f18352c0a1dc30a7fa95503a5d50fc4bed3
Praveen Dias <praveendias1180@gmail.com>
2021-12-03 10:48:43 +0530

    Setup for all tests.

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -3,6 +3,14 @@ use PHPUnit\Framework\TestCase;
 use App\Cart;
 
 class CartTest extends TestCase{
+
+    protected $cart;
+
+    protected function setUp(): void 
+    {
+        $this->cart = new Cart();
+    }
+
     function testCalculateFinal(){
         $cart = new Cart();
         $final = $cart->calculateFinal(10);
